709a45a9af86b133c526634ed18189bd6f5ff483
ameena117 <ameenajadallah@gmail.com>
2024-02-01 00:26:21 +0200

    edits

--- detail.js
+++ detail.js
@@ -3,46 +3,36 @@ console.log(id);
 let details = document.querySelector(".details");
 let detailsByAxios = document.querySelector(".detailsByAxios");
 
-
 async function getProduct(id) {
     const response = await fetch(`https://dummyjson.com/products/${id}`);
     let product = await response.json();
     console.log(product);
-    details.innerHTML = 
-    `
-    <div class="product">
-    <h2>${product.title}</h2>
-    <p>${product.description}</p>
-    <div class="images">
-    ${product.images.map(image => `<img src="${image}" alt="img">`).join(' ')}
-    <div class="images">
-    </div>
-    `   
-}
+    details.innerHTML = `<div class="product">
+        <h2>${product.title}</h2>
+        <p>${product.description}</p>
+        <div class="images">
+        ${product.images.map(image => `<img src="${image}" alt="img">`).join(' ')}
+        </div>
+    </div>`}
 getProduct(id);
 
 async function getProductByAxios(id) {
 
     const productByAxios =
-    await axios.get(`https://dummyjson.com/products/${id}`)
-        .then((response) => {
-            return response.data;
-        })
-        .catch((error) => {
-            console.log(error);
-            return [];
-        });
+        await axios.get(`https://dummyjson.com/products/${id}`)
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+                return [];
+            });
     console.log(productByAxios);
-    detailsByAxios.innerHTML = 
-    `
-    <div class="product">
-    <h2>${productByAxios.title} By Axios</h2>
-    <p>${productByAxios.description}</p>
-    <div class="images">
-    ${productByAxios.images.map(image => `<img src="${image}" alt="img">`).join(' ')}
-    </div>
-    </div>
-    `   
-   
-}
+    detailsByAxios.innerHTML = `<div class="product">
+        <h2>${productByAxios.title} By Axios</h2>
+        <p>${productByAxios.description}</p>
+        <div class="images">
+        ${productByAxios.images.map(image => `<img src="${image}" alt="img">`).join(' ')}
+        </div>
+    </div>`}
 getProductByAxios(id);
--- main.js
+++ main.js
@@ -1,50 +1,40 @@
-
 async function getProducts() {
     const response = await fetch("https://dummyjson.com/products");
     let products = await response.json();
     products = products.products;
     console.log(products);
     const result = products.map(function (product) {
-        return `
-        
-
-        <div class="product">
+        return `<div class="product">
         <img src="${product.thumbnail}"</img>
                 <h3>${product.title}</h3>
                 <h4>${product.price} $</h4>
                 <a href="product.html?id=${product.id}">See More</a>
-            </div>
-        `
-    }
-    ).join(' ');
-    document.querySelector(".products .container .row").innerHTML = result;
+        </div>`
+    }).join(' ');
+    document.querySelector(".products .row").innerHTML = result;
 }
 getProducts();
 
 async function getProductsByAxios() {
     const productsByAxios =
-    await axios.get("https://dummyjson.com/products")
-        .then((response) => {
-            return response.data.products;
-        })
-        .catch((error) => {
-            console.log(error);
-            return [];
-        });
-    console.log(productsByAxios);    
+        await axios.get("https://dummyjson.com/products")
+            .then((response) => {
+                return response.data.products;
+            })
+            .catch((error) => {
+                console.log(error);
+                return [];
+            });
+    console.log(productsByAxios);
 
     const result = productsByAxios.map(function (product) {
-        return `
-
-            <div class="product">
+        return `<div class="product">
                 <img src="${product.thumbnail}"</img>
                 <h3>${product.title}</h3>
                 <h4>${product.price} $</h4>
                 <a href="product.html?id=${product.id}">See More</a>
-            </div>
-        `
-    }
-    ).join(' ');
-    document.querySelector(".productsByAxios .container .row").innerHTML = result;
+        </div>`
+    }).join(' ');
+    document.querySelector(".productsByAxios .row").innerHTML = result;
 }
 getProductsByAxios();
