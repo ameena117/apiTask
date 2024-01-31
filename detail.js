4db8df7740c5f6d6fbd00254379f17893e782faf
ameena117 <ameenajadallah@gmail.com>
2024-01-31 11:25:07 +0200

    axios

--- detail.js
+++ detail.js
@@ -1,6 +1,8 @@
 let id = new URLSearchParams(window.location.search).get("id");
 console.log(id);
 let details = document.querySelector(".details");
+let detailsByAxios = document.querySelector(".detailsByAxios");
+
 
 async function getProduct(id) {
     const response = await fetch(`https://dummyjson.com/products/${id}`);
@@ -13,9 +15,31 @@ async function getProduct(id) {
     <p>${product.description}</p>
     ${product.images.map(image => `<img src="${image}" alt="img">`).join(' ')}
     </div>
-    `
-    
-    
+    `   
 }
-
 getProduct(id);
+
+async function getProductByAxios(id) {
+
+    const productByAxios =
+    await axios.get(`https://dummyjson.com/products/${id}`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+            return [];
+        });
+    console.log(productByAxios);
+    detailsByAxios.innerHTML = 
+    `
+    <div>
+    <h2>${productByAxios.title}</h2>
+    <p>${productByAxios.description}</p>
+    ${productByAxios.images.map(image => `<img src="${image}" alt="img">`).join(' ')}
+    </div>
+    `   
+    
+   
+}
+getProductByAxios(id);
